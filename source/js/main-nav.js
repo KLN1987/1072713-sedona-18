91067e9660d74a7ee5401aa2acb4827b56e034b5
Lev Karpov <nt66@bk.ru>
2019-12-27 02:25:51 +0300

    Отправляет на проверку Адаптивные декор.элеметы исправления p.6

--- source/js/main-nav.js
+++ source/js/main-nav.js
@@ -1,18 +1,7 @@
 var navMain = document.querySelector('.main-nav');
 var navBtn = document.querySelector('.main-nav__btn');
-var headerBtn = document.querySelector('.page-header__btn');
 
 navMain.classList.remove('main-nav--nojs');
-
-headerBtn.addEventListener('click', function() {
-  if (navMain.classList.contains('main-nav--closed')) {
-    navMain.classList.remove('main-nav--closed');
-    navMain.classList.add('main-nav--opened');
-  } else {
-    navMain.classList.add('main-nav--closed');
-    navMain.classList.remove('main-nav--opened');
-  }
-});
 
 navBtn.addEventListener('click', function() {
   if (navMain.classList.contains('main-nav--closed')) {
