62cbe9997b11ec73be10fc66a5b3c563bc7847a0
Lev Karpov <nt66@bk.ru>
2020-01-23 13:53:59 +0300

    Отправляет на проверку Погружение в автоматизацию исправления p.6

--- gulpfile.js
+++ gulpfile.js
@@ -32,6 +32,22 @@ gulp.task("copy", function () {
     .pipe(gulp.dest("build"));
   });
 
+gulp.task("images", function() {
+  return gulp.src("source/img/**/*.{png,jpg,svg}")
+    .pipe(imagemin([
+      imagemin.optipng({optimizationLevel: 3}),
+      imagemin.jpegtran({progressive: true}),
+      imagemin.svgo()
+    ]))
+    .pipe(gulp.dest("build/img"));
+  });
+
+gulp.task("webp", function() {
+  return gulp.src("source/img/**/*.{png,jpg}")
+    .pipe(webp({quality: 80}))
+    .pipe(gulp.dest("build/img"));
+});
+
 gulp.task("css", function () {
   return gulp.src("source/less/style.less")
     .pipe(plumber())
@@ -60,22 +76,6 @@ gulp.task("html", function () {
     .pipe(gulp.dest("build"));
 });
 
-gulp.task("images", function() {
-  return gulp.src("source/img/**/*.{png,jpg,svg}")
-    .pipe(imagemin([
-      imagemin.optipng({optimizationLevel: 3}),
-      imagemin.jpegtran({progressive: true}),
-      imagemin.svgo()
-    ]))
-    .pipe(gulp.dest("build/img"));
-});
-
-gulp.task("webp", function() {
-  return gulp.src("source/img/**/*.{png,jpg}")
-    .pipe(webp({quality: 80}))
-    .pipe(gulp.dest("build/img"));
-});
-
 gulp.task("server", function () {
   server.init({
     server: "build/"
@@ -90,5 +90,5 @@ gulp.task("refresh", function (done) {
   done();
 });
 
-gulp.task("build", gulp.series("clean", "copy", "css", "sprite", "html"));
+gulp.task("build", gulp.series("clean", "copy", "images", "webp", "css", "sprite", "html"));
 gulp.task("start", gulp.series("build", "server"));
